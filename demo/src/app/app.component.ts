342cc553f9b6cee807c84a507171526c99fd93b3
Roma Basarab <romabasarab@MacBook-Pro-Roma.local>
2026-03-05 22:46:09 +0200

    feat: Added ui-kit

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UiModule } from '@chortex/ui-kit-lib';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UiModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'demo';
-}
+export class AppComponent {}
--- demo/src/app/app.config.ts
+++ demo/src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { IconsPreloaderService, UiModule } from '@chortex/ui-kit-lib';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), importProvidersFrom(UiModule), IconsPreloaderService],
 };
