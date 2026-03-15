e5afac226817df543b2203c279ca732ee9413f66
Roma Basarab <romabasarab@MacBook-Pro-Roma.local>
2026-03-15 22:35:22 +0200

    fix: Fixed sidebar routing

--- demo/src/app/app.routes.ts
+++ demo/src/app/app.routes.ts
@@ -4,16 +4,32 @@ import { MapDemoComponent } from './components/map-demo/map-demo.component';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/demo/map',
     pathMatch: 'full',
+    redirectTo: 'demo',
   },
   {
-    path: 'demo/map',
-    component: MapDemoComponent,
-    title: 'HL Maps - Basic Demo',
+    path: 'demo',
+    title: 'HL Maps - Demo',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'map-basic',
+      },
+      {
+        path: 'map-basic',
+        component: MapDemoComponent,
+        title: 'HL Maps - Basic Map',
+      },
+    ],
   },
+  // {
+  //   path: 'docs',
+  //   loadComponent: () => import('./docs/docs.component').then(m => m.DocsComponent),
+  //   title: 'HL Maps – Documentation',
+  // },
   {
     path: '**',
-    redirectTo: '/demo/map',
+    redirectTo: 'demo',
   },
 ];
--- demo/src/app/components/sidebar/sidebar.component.ts
+++ demo/src/app/components/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { UiModule } from '@chortex/ui-kit-lib';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [UiModule],
+  imports: [UiModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
