d1289ef91a2299b57cb646d8c1a86a27f7ecc3e9
Roma Basarab <romabasarab@MacBook-Pro-Roma.local>
2026-03-16 23:49:28 +0200

    fix: Added layer component to public api

--- ui/src/public-api.ts
+++ ui/src/public-api.ts
@@ -1,5 +1,6 @@
 // Components
 export * from './lib/components/map/map.component';
+export * from './lib/components/layer/layer.component';
 
 // Services
 export * from './lib/services/map.service';
